7ffc59824a25ee8d8a040309d2c67b2dc2728255
souvikmndl <svk72mi@gmail.com>
2026-01-12 18:51:54 +0530

    reordering queries to avoid deadlocks

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -49,7 +49,7 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 type TransferTxParams struct {
 	FromAccountID int64 `json:"from_account_id"`
 	ToAccountID   int64 `json:"to_account_id"`
-	Amout         int64 `json:"amount"`
+	Amount        int64 `json:"amount"`
 }
 
 // TransferTxResult is the result of the transfer tx
@@ -61,15 +61,81 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
+// We need to pass a unique name(key,value) to each tx via ctx for debugging
+// we can't pass a normal built in type as the key, so using struct
+var txKey = struct{}{}
+
 // TransferTx performs a money transfer from one account to the other
 // It creates a transfer record, add account entries, and updates accounts' balance in a single db tx
-// func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-//     var result TransferTxResult
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
 
-//     err := store.execTx(ctx, func(q *Queries) error {
-//         result.Transfer, err = q.CreateTra
-//         return nil
-//     })
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
 
-//     reutrn result, err
-// }
+		//txName := ctx.Value(txKey) --> tx name used for debugging deadlocks
+		// create the transfer data
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// create entry data for money leaving from_account
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount, // -ve because money is leaving
+		})
+
+		// create entry data for money entering to_account
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+
+		// if we are exchanging money betn acc1 and acc2 concurrently, we must make sure that only
+		// one table is being updated in both(or all) txs at the same time. That will ensure lock
+		// is acheived, successive queries are executed and then commited followed by other txs
+		// Else in one tx we will update acc1-->acc2 and in the other we update acc2-->acc1, they
+		// will depend on each other to release the locks creating a deadlock
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
+
+		return nil
+	})
+
+	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1,
+	amount1,
+	accountID2,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return // we have named params so no need to specify var names here
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ db/sqlc/store_test.go
@@ -0,0 +1,177 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> Before:", account1.Balance, account2.Balance)
+
+	// best way to test transactions is conncurrently, as that is their purpose
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	// firing up n concurrent transactions on the same accounts
+	for i := 0; i < n; i++ {
+		txName := fmt.Sprintf("tx %d", i+1)
+		go func() {
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			result, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err       // send err into channel
+			results <- result // send result
+		}()
+	}
+
+	// check results
+	existed := make(map[int]bool)
+
+	for i := 0; i < n; i++ {
+		err := <-errs // reading from err chan
+		require.NoError(t, err)
+
+		result := <-results // reading results
+		require.NotEmpty(t, result)
+
+		// check transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		// fetching to see if it really got created
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		// checking updated account balances, no need to check get here
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check balances
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount, ..., n * amount
+
+		k := int(diff1 / amount) // k tracks i+1
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k) // k will be unique for each iteration
+		existed[k] = true
+	}
+
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+
+	fmt.Println(">> After:", account1.Balance, account2.Balance)
+}
+
+// TestTransferTxDeadlock runs concurrent transactions sending money from one account to another
+// and vice versa as well. The prev test above was only sending money from acc1 to acc2 but this
+// will send money from acc1 to acc2 and from acc2 to acc1 concurrently
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> Before:", account1.Balance, account2.Balance)
+
+	// best way to test transactions is conncurrently, as that is their purpose
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	// firing up n concurrent transactions on the same accounts
+	for i := 0; i < n; i++ {
+		txName := fmt.Sprintf("tx %d", i+1)
+
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err // send err into channel
+		}()
+	}
+
+	// check for deadlocks
+	for i := 0; i < n; i++ {
+		err := <-errs // reading from err chan
+		require.NoError(t, err)
+	}
+
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	// since 5 times we sent amount from acc1 to acc2 and then vice versa, balances will be same
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+
+	fmt.Println(">> After:", account1.Balance, account2.Balance)
+}
